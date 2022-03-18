9c0f72300669b8b5b85157790c5a1af3402ba33a
Zu3s <91681532+Zu3s@users.noreply.github.com>
2022-03-18 19:22:02 +0000

    Update Troll.lua

---
+++ Troll.lua
@@ -0,0 +1,31 @@
+local Plr = game:GetService("Players").LocalPlayer --[[Player]]--
+local Char = Plr.Character or Plr.CharacterAdded:Wait() --[[Character]]--
+
+function Skid() --[[Fuck The Skids]]--
+
+    --[[Flight Exploits]]--
+    Instance.new("BodyVelocity").Parent = Char
+    Instance.new("BodyGyro").Parent = Char
+    Instance.new("BodyForce").Parent = Char
+
+    --[[Speed Exploits]]--
+    Char.Humanoid.WalkSpeed = 999999999999999999999 
+
+    --[[Heath Exploits]]--
+    Char.Humanoid.MaxHealth = math.huge
+
+
+    task.wait(3) -- wait 3 seconds
+
+    Char.Parent = nil -- deletes character
+
+    task.wait(1) -- wait 1 second
+
+    Plr.Parent = nil -- deletes player
+    
+    task.wait(2) -- wait 2 seconds
+    --[[Crash]]--
+    while true do end 
+end -- function end
+
+game:GetService("RunService").RenderStepped:Connect(Skid) --[[HEHEHEHEHHEHE]]--
